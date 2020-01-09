562e076673c57f456b18feafe14bb7bc129df9e7
Pedro Masamuna <pmasamuna@MacBook-Air-de-Pedro-Masamuna.local>
2020-01-09 19:53:50 +0100

    New class removed

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -10,13 +10,4 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 	
-	public class ObjectWithNonPureFunction{
-	    private int value = 0;
-
-	    public int add(int nextValue) {
-	        this.value += nextValue;
-	        return this.value;
-	    }
-	}
-
 }
