1f8c6cbc475554e890b707dd205536c8b93b1e1e
Pedro Masamuna <pmasamuna@MacBook-Air-de-Pedro-Masamuna.local>
2020-01-09 19:48:16 +0100

    New class added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,5 +9,14 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	public class ObjectWithNonPureFunction{
+	    private int value = 0;
+
+	    public int add(int nextValue) {
+	        this.value += nextValue;
+	        return this.value;
+	    }
+	}
 
 }
